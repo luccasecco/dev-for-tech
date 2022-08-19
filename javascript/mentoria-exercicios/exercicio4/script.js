d0e9e56b2aa8d8a1aa814c2ca693cf585deba502
luccasecco <luccasecco@hotmail.com>
2022-08-19 20:24:26 -0300

    add validação

--- javascript/mentoria-exercicios/exercicio4/script.js
+++ javascript/mentoria-exercicios/exercicio4/script.js
@@ -1,6 +1,11 @@
 function triangle() {
   let number = Number(prompt('Digite uma quantidade'))
 
+  while (isNaN(number)) {
+    alert('Por favor digite um número inteiro')
+    number = Number(prompt('Digite uma quantidade'))
+  }
+
   for (let i = 0; i <= number; i++) {
     let result = ''
     for (let j = 0; j < i; j++) {
