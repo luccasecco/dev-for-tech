03b04d6d51e4b87f369924cc28aeb18f6a61a000
luccasecco <luccasecco@hotmail.com>
2022-08-19 19:46:14 -0300

    add exercicio 3

---
+++ javascript/mentoria-exercicios/exercicio3/script.js
@@ -0,0 +1,21 @@
+function isNumberEvenOrOdd() {
+  let number = Number(prompt('Digite um número'))
+  let result = 0
+
+  while (isNaN(number)) {
+    alert('Valor inválido. Por favor digite um número inteiro')
+    number = Number(prompt('Digite um número'))
+  }
+
+  if (number % 2 === 0) {
+    result = `O número ${number} é par`
+  } else {
+    result = `O número ${number} é ímpar`
+  }
+
+  document.querySelector('#result').innerHTML = `
+  <p>Número: <strong>${number}</strong></p>
+  <br />
+  <h3>Resultado: ${result}</h3>
+`
+}
