f9f33d92c2f934e2cd650a44bce289df3ca363b5
luccasecco <luccasecco@hotmail.com>
2022-08-23 19:42:52 -0300

    add correção de cores

--- javascript/mentoria-exercicios/consultaCEP/scripts.js
+++ javascript/mentoria-exercicios/consultaCEP/scripts.js
@@ -1,8 +1,4 @@
 const button = document.getElementById('send')
-const cep = document.querySelector('#cepNumber')
-const street = document.querySelector('#street')
-const district = document.querySelector('#district')
-const local = document.querySelector('#local')
 const table = document.querySelector('#table-cep')
 const box = document.querySelector('#results')
 const emptyText = document.querySelector('#empty-text')
@@ -10,17 +6,39 @@ const buttonClear = document.querySelector('#button-clear')
 
 const arrDataBase = []
 
+const getDataBase = () => {
+  JSON.parse(localStorage.getItem('@consulta_CEP')) ?? []
+}
+
+const setDataBase = arrDataBase => {
+  localStorage.setItem('@consulta_CEP', JSON.stringify(arrDataBase))
+}
+
+const removeItems = () => {
+  buttonClear.classList.add('hide')
+  table.classList.add('hide')
+  emptyText.style.display = 'block'
+
+  localStorage.removeItem('@consulta_CEP: ')
+}
+
 const setTable = arrDataBase => {
+  const item = document.querySelector('#table-body')
   buttonClear.classList.remove('hide')
   emptyText.style.display = 'none'
   table.classList.remove('hide')
 
-  arrDataBase.map(item => {
-    cep.innerText = item.cep
-    street.innerText = item.logradouro
-    district.innerText = item.bairro
-    local.innerText = item.localidade + '/' + item.uf
-  })
+  for (let i = 0; i < arrDataBase.length; i++) {
+    item.innerHTML = `
+    <tr id="${arrDataBase[i]}">
+      <td>${arrDataBase[i].cep}</td>
+      <td>${arrDataBase[i].logradouro}</td>
+      <td>${arrDataBase[i].bairro}</td>
+      <td>${arrDataBase[i].localidade} / ${arrDataBase[i].uf}</td>
+    </tr>
+    `
+    document.querySelector('#table-cep').appendChild(item)
+  }
 }
 
 const getCep = async () => {
@@ -31,12 +49,17 @@ const getCep = async () => {
   const data = await api.json()
 
   arrDataBase.push(data)
-  localStorage.setItem('@consulta_CEP: ', JSON.stringify(arrDataBase))
 
-  // const getLocalStorage = localStorage.getItem('@consulta_CEP: ')
-  // const dataBase = JSON.parse(getLocalStorage)
+  // localStorage.setItem('@consulta_CEP: ', localStorageData)
 
+  // if (localStorage) {
+  //   const getLocalStorage = localStorage.getItem(arrDataBase)
+  //   const dataBase = JSON.parse(getLocalStorage)
+  //   arrDataBase.push(dataBase)
+  // }
+  setDataBase(arrDataBase)
   setTable(arrDataBase)
 }
 
 button.addEventListener('click', getCep)
+buttonClear.addEventListener('click', removeItems)
