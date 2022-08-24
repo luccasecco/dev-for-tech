bf1f418859db71e43b40bfa60b17302381775ec1
luccasecco <luccasecco@hotmail.com>
2022-08-24 16:40:53 -0300

    versão 1.0

--- javascript/mentoria-exercicios/consultaCEP/scripts.js
+++ javascript/mentoria-exercicios/consultaCEP/scripts.js
@@ -1,65 +1,57 @@
+const cep = document.getElementById('cep')
 const button = document.getElementById('send')
-const table = document.querySelector('#table-cep')
+const table = document.querySelector('#card-body')
 const box = document.querySelector('#results')
 const emptyText = document.querySelector('#empty-text')
-const buttonClear = document.querySelector('#button-clear')
+const buttonClear = document.querySelector('#btn-clear')
 
-const arrDataBase = []
-
-const getDataBase = () => {
-  JSON.parse(localStorage.getItem('@consulta_CEP')) ?? []
-}
-
-const setDataBase = arrDataBase => {
-  localStorage.setItem('@consulta_CEP', JSON.stringify(arrDataBase))
-}
+let arrDataBase = JSON.parse(localStorage.getItem('dataBase')) ?? []
 
 const removeItems = () => {
-  buttonClear.classList.add('hide')
-  table.classList.add('hide')
-  emptyText.style.display = 'block'
-
-  localStorage.removeItem('@consulta_CEP: ')
+  if (arrDataBase.length > 0) {
+    arrDataBase = []
+    localStorage.removeItem('dataBase')
+    // buttonClear.classList.add('hide')
+    table.classList.add('hide')
+    emptyText.style.display = 'block'
+  }
+  // localStorage.removeItem(arrDataBase)
 }
 
-const setTable = arrDataBase => {
-  const item = document.querySelector('#table-body')
-  buttonClear.classList.remove('hide')
-  emptyText.style.display = 'none'
-  table.classList.remove('hide')
+const setTable = () => {
+  arrDataBase = JSON.parse(localStorage.getItem('dataBase'))
 
-  for (let i = 0; i < arrDataBase.length; i++) {
-    item.innerHTML = `
-    <tr id="${arrDataBase[i]}">
-      <td>${arrDataBase[i].cep}</td>
-      <td>${arrDataBase[i].logradouro}</td>
-      <td>${arrDataBase[i].bairro}</td>
-      <td>${arrDataBase[i].localidade} / ${arrDataBase[i].uf}</td>
-    </tr>
-    `
-    document.querySelector('#table-cep').appendChild(item)
+  // const item = document.querySelector('#table-body')
+
+  if (arrDataBase) {
+    // buttonClear.classList.remove('hide')
+    emptyText.style.display = 'none'
+    table.classList.remove('hide')
+
+    arrDataBase.map(data => {
+      table.innerHTML = `
+          <button id="btn-clear" onClick=removeItems()><i class="icon-trash"></i></button>
+          <p>CEP</p> <span>${data.cep}</span><hr />
+          <p>ENDEREÇO</p> <span>${data.logradouro}</span><hr />
+          <p>BAIRRO</p> <span>${data.bairro}</span><hr />
+          <p>CIDADE</p> <span>${data.localidade}</span><hr />
+          <p>ESTADO</p> <span>${data.uf}</span><hr />
+       
+        `
+    })
   }
 }
 
 const getCep = async () => {
-  const cep = document.getElementById('cep').value
-  const url = `https://viacep.com.br/ws/${cep}/json/`
+  let cepInput = cep.value.replace('-', '')
+  const url = `https://viacep.com.br/ws/${cepInput}/json/`
 
-  const api = await fetch(url)
-  const data = await api.json()
+  await fetch(url)
+    .then(response => response.json())
+    .then(data => arrDataBase.push(data))
 
-  arrDataBase.push(data)
-
-  // localStorage.setItem('@consulta_CEP: ', localStorageData)
-
-  // if (localStorage) {
-  //   const getLocalStorage = localStorage.getItem(arrDataBase)
-  //   const dataBase = JSON.parse(getLocalStorage)
-  //   arrDataBase.push(dataBase)
-  // }
-  setDataBase(arrDataBase)
+  localStorage.setItem('dataBase', JSON.stringify(arrDataBase))
   setTable(arrDataBase)
 }
 
 button.addEventListener('click', getCep)
-buttonClear.addEventListener('click', removeItems)
