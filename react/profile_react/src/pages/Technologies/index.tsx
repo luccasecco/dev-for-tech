70ee67cae316ce3947a6b1882c572b33126267f7
luccasecco <luccasecco@hotmail.com>
2022-09-11 08:55:03 -0300

    add pagina tecnologias

--- react/profile_react/src/pages/Technologies/index.tsx
+++ react/profile_react/src/pages/Technologies/index.tsx
@@ -1,10 +1,21 @@
-
-import { Container } from "./styles";
+import { ArrowArcLeft } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+import { Container, Content } from "./styles";
 
 export function Technologies() {
   return (
     <Container>
+      <Content>
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg" alt="HTML5" />
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg" alt="CSS3" />
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg" alt="REACT" />
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-plain.svg" alt="TYPESCRIPT" />
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg" alt="JAVASCRIPT" />
+      </Content>
 
+      <div className="back-to-home">
+        <NavLink to="/"><ArrowArcLeft size={25} />Início</NavLink>
+      </div>
 
     </Container>
   )
--- react/profile_react/src/pages/Technologies/styles.ts
+++ react/profile_react/src/pages/Technologies/styles.ts
@@ -3,9 +3,31 @@ import styled from 'styled-components'
 export const Container = styled.div`
   max-width: 75rem;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  flex-direction: column;
+  gap: 2rem;
   margin: 5rem auto;
 
+  .back-to-home {
+    a{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      color: ${props => props.theme['gray-100']}
+    }
+  }
+`
+
+export const Content = styled.div`
+  max-width: 75rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  margin: 5rem auto;
+
+  img {
+    width: 5rem;
+  }
 `
